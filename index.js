e0a5ccc0d7d00f57501c9bc3fb92f5cd10e8323b
adityasunny1189 <adityapathak1189@gmail.com>
2019-11-09 17:00:30 +0530

    jff

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("hello");
+
+document.querySelector("#love-button").addEventListener("click", function() {
+  document.querySelector(".first").style.display = "flex";
+});
+
+function() {
+  alert("Helllo");
+}
